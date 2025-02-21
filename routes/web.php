c16542bccbe33c497d006160826d9150548fbd3c
vitor-carvalho-ddt <vitor.carvalho@kanastra.com.br>
2025-02-21 19:22:13 -0300

    Adding simple users listing route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
+Route::get(uri: '/users', action: [UserController::class, 'index'])->name(name: 'users.index');
+
+Route::get(uri: '/', action: function (){
     return view('welcome');
 });
