384bf5c586b30e96b98895edc43bd778bce3f448
clib <vitor.carvalho.ufu@gmail.com>
2025-02-23 02:11:24 -0300

    Cep table migration

---
+++ database/migrations/2025_02_23_025754_create_ceps_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ceps', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('cep', 9)->nullable();
+            $table->string('logradouro')->nullable();
+            $table->string('complemento')->nullable();
+            $table->string('unidade')->nullable();
+            $table->string('bairro')->nullable();
+            $table->string('localidade')->nullable();
+            $table->string('uf', 2)->nullable();
+            $table->string('estado')->nullable();
+            $table->string('regiao')->nullable();
+            $table->string('ibge')->nullable();
+            $table->string('gia')->nullable();
+            $table->string('ddd')->nullable();
+            $table->string('siafi')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ceps');
+    }
+};
