447d150ab05759e46d8449664614a1f8435e8d2d
clib <vitor.carvalho.ufu@gmail.com>
2025-02-23 02:03:19 -0300

    Adding ceps resource routing

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,25 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
+use \App\Http\Controllers\CepController;
 
-Route::get(uri: '/users', action: [UserController::class, 'index'])->name(name: 'users.index');
-
-Route::get(uri: '/', action: function (){
-    return view('welcome');
+Route::middleware('auth')->group(function() {
+    Route::resource('ceps', CepController::class);
+    Route::get('/', [CepController::class,'index']);
 });
+
+
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
