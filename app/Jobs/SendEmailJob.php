13234878bba07053658a918c06a577a0db5fcecc
vitor-carvalho-ddt <vitor.carvalho.ufu@gmail.com>
2025-03-24 09:52:37 -0300

    Adding Send Email Job

---
+++ app/Jobs/SendEmailJob.php
@@ -0,0 +1,25 @@
+<?php
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\SendEmailTest;
+
+class SendEmailJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $details;
+
+    public function __construct($details)
+    {
+        $this->details = $details;
+    }
+
+    public function handle()
+    {
+        Mail::to($this->details['email'])->send(new SendEmailTest($this->details));
+    }
+}
